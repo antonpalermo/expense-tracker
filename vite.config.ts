1fa88488d55a6bb839047d5708637b8f2c45043f
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-01-04 22:27:39 +0800

    enable import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite"
 
+import path from "node:path"
 import react from "@vitejs/plugin-react"
 
 import { cloudflare } from "@cloudflare/vite-plugin"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()]
+  plugins: [react(), cloudflare()],
+  resolve: {
+    alias: {
+      "@workers": path.resolve(__dirname, "./workers")
+    }
+  }
 })
