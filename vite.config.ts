28998b619b83b9e751a6ffad5628cb383877e739
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-01-09 17:48:44 +0800

    setup tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,26 @@ import path from "node:path"
 import react from "@vitejs/plugin-react"
 
 import { cloudflare } from "@cloudflare/vite-plugin"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "./www/routes",
+      generatedRouteTree: "./www/routeTree.gen.ts"
+    }),
+    react(),
+    cloudflare()
+  ],
   resolve: {
     alias: {
       "@workers": path.resolve(__dirname, "./workers")
     }
+  },
+  optimizeDeps: {
+    exclude: ["@tanstack/react-router-devtools"]
   }
 })
