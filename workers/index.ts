207943a4d0cb0db86657c084e0bcdb4ee3f36ae5
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-01-12 23:23:24 +0800

    used session and db middleware

--- workers/index.ts
+++ workers/index.ts
@@ -2,12 +2,17 @@ import { Hono } from "hono"
 
 import type { AppBindings } from "@workers/lib/types"
 
-import { notFound } from "@workers/middlewares/not-found"
 import { configureBetterAuth } from "@workers/lib/configure-better-auth"
+
+import { session } from "@workers/middlewares/session"
+import { database } from "@workers/middlewares/database"
+import { notFound } from "@workers/middlewares/not-found"
 
 const app = new Hono<AppBindings>().basePath("/api")
 
 configureBetterAuth(app)
+
+app.use(session).use(database)
 
 app.notFound(notFound)
 
