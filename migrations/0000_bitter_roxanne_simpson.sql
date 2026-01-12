d61abdc4abc71abbed9bbf57430e8743a07fb199
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-01-12 23:41:45 +0800

    migrate to open-next

--- migrations/0000_bitter_roxanne_simpson.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "transactions" (
-	"id" uuid DEFAULT gen_random_uuid(),
-	"name" text,
-	"description" text,
-	"dateCreated" timestamp DEFAULT now(),
-	"dateUpdated" timestamp,
-	CONSTRAINT "transactions_id_unique" UNIQUE("id")
-);
--- www/lib/auth.ts
+++
@@ -1,5 +0,0 @@
-import { createAuthClient } from "better-auth/react"
-
-export const { signIn, signOut, useSession, $Infer } = createAuthClient()
-
-export type Session = typeof $Infer.Session
