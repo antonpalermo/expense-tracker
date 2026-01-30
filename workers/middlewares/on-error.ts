10f94d2e6a32ca2bc108ece9a6d28c6200871767
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-01-30 21:30:49 +0800

    change on-error default response

--- workers/middlewares/on-error.ts
+++ workers/middlewares/on-error.ts
@@ -2,11 +2,17 @@ import type { ErrorHandler } from "hono"
 import { HTTPException } from "hono/http-exception"
 import type { ContentfulStatusCode } from "hono/utils/http-status"
 
+import * as HTTPStatus from "../status-codes"
+import * as HTTPPhrases from "../status-phrases"
+
 export const onError: ErrorHandler = (err, ctx) => {
   if (err instanceof HTTPException) {
     const res = err.getResponse()
     return ctx.json({ message: err.cause }, res.status as ContentfulStatusCode)
   }
 
-  return ctx.json({ message: err.cause })
+  return ctx.json(
+    { message: HTTPPhrases.INTERNAL_SERVER_ERROR },
+    HTTPStatus.INTERNAL_SERVER_ERROR
+  )
 }
