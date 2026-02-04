39117177b48c72e4b14ec35d4e9f02a578146346
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-02-04 17:33:54 +0800

    generate metadata migrations

---
+++ migrations/0003_new_star_brand.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "metadata" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"defaults" json,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "metadata_id_unique" UNIQUE("id")
+);
+--> statement-breakpoint
+ALTER TABLE "metadata" ADD CONSTRAINT "metadata_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "metadata_id_idx" ON "metadata" USING btree ("id");--> statement-breakpoint
+CREATE INDEX "metadata_userId_idx" ON "metadata" USING btree ("user_id");
