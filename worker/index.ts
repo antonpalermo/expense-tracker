7b60a7d6c6d358445e5646a3ebd4f4a856f2c1f4
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-02-28 20:53:56 +0800

    refac: update imports and cleanup workers

--- worker/index.ts
+++ worker/index.ts
@@ -1,30 +1,10 @@
 import { Hono } from "hono"
 import { logger } from "hono/logger"
 import { secureHeaders } from "hono/secure-headers"
-import { db } from "./database/db"
-import { transaction } from "./database/schemas/transaction"
 
 const app = new Hono({ strict: false }).basePath("/api")
 
 app.use(logger())
 app.use(secureHeaders())
-
-app.get("/status", async ctx => {
-  return ctx.json({ msg: "ok" })
-})
-
-app.post("/create", async ctx => {
-  const result = await db
-    .insert(transaction)
-    .values({ name: "shambal" })
-    .returning()
-
-  return ctx.json({ transaction: result[0] })
-})
-
-app.get("/transactions", async ctx => {
-  const result = await db.select().from(transaction)
-  return ctx.json(result)
-})
 
 export default app
