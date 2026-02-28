e8a7ad19a7e47acef7a5f6fc2acc0d8f26d20754
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-02-28 23:02:14 +0800

    refac: merge auth schema into single file

--- worker/lib/auth.ts
+++ worker/lib/auth.ts
@@ -3,16 +3,26 @@ import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 
 import { db } from "@workers/database/db"
-import * as schemas from "@workers/database/schemas"
+import * as schemas from "@workers/database/schemas/auth"
 
+// temporarily comment out this constant before
+// generating auth database tables.
 const auth = betterAuth({
   appName: "xpens",
-  baseURL: env.BETTER_AUTH_URL, // NOTE: comment out before generating the better-auth schema
-  secret: env.BETTER_AUTH_SECRET, // NOTE: comment out before generating the better-auth schema
+  baseURL: env.BETTER_AUTH_URL,
+  secret: env.BETTER_AUTH_SECRET,
   database: drizzleAdapter(db, {
     provider: "sqlite",
     schema: schemas
   })
 })
+// comment until here!
+
+// uncomment this constant when generating schema
+// const auth = betterAuth({
+//   baseURL: "http://localhost:5173",
+//   secret: "your secret here!",
+//   database: drizzleAdapter(db, { provider: "sqlite" })
+// })
 
 export default auth
