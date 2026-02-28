39da9b46942d742cc90d7d7d54b6d68dde9acc99
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-02-28 23:57:33 +0800

    added basic auth flow.

--- www/routes/index.tsx
+++ www/routes/index.tsx
@@ -1,11 +1,50 @@
-import { createFileRoute } from "@tanstack/react-router"
+import {
+  createFileRoute,
+  redirect,
+  useLocation,
+  useNavigate
+} from "@tanstack/react-router"
 
 import App from "@client/components/app"
+import { authClient, logout } from "@client/lib/auth-client"
 
 export const Route = createFileRoute("/")({
-  component: RouteComponent
+  component: RouteComponent,
+  beforeLoad: async ({ location }) => {
+    const { data: session } = await authClient.getSession()
+
+    if (!session) {
+      throw redirect({
+        to: "/login",
+        search: {
+          redirect: location.href
+        },
+        replace: true
+      })
+    }
+  }
 })
 
 function RouteComponent() {
-  return <App />
+  const href = useLocation({
+    select: location => location.href
+  })
+  const navigate = useNavigate({
+    from: "/"
+  })
+
+  return (
+    <div>
+      <App />
+      <button
+        onClick={async () =>
+          await logout(() => {
+            navigate({ to: "/login", search: { redirect: href } })
+          })
+        }
+      >
+        Sign Out
+      </button>
+    </div>
+  )
 }
