0fcb267e593fa5e3743978da4089e40b95f17b49
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-03-01 11:15:28 +0800

    added session middleware

--- worker/index.ts
+++ worker/index.ts
@@ -3,11 +3,14 @@ import { logger } from "hono/logger"
 import { secureHeaders } from "hono/secure-headers"
 
 import authRoutes from "@workers/routes/auth.route"
+import authGuard from "@workers/middlewares/auth-guard"
 
 const app = new Hono({ strict: false }).basePath("/api")
 
 app.use(logger())
 app.use(secureHeaders())
+
+app.use(authGuard)
 
 const routes = [authRoutes]
 
