9d5ad18c6ce96922eda5492da4738f2a0c44a193
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-03-04 17:19:34 +0800

    refac: update hono types

--- worker/routes/auth.route.ts
+++ worker/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono"
 
+import type { AppBindings } from "@workers/types"
 import auth from "@workers/lib/auth"
 
-const routes = new Hono<{ Bindings: CloudflareBindings }>().basePath("/auth")
+const routes = new Hono<AppBindings>().basePath("/auth")
 
 routes.on(["GET", "POST"], "*", async ctx => {
   return auth.handler(ctx.req.raw)
