d8844d1c12cc7d95804d7c5b2d7e14307b17e6b1
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-03-06 22:11:53 +0800

    added route validation

--- worker/handlers/entry.handler.ts
+++ worker/handlers/entry.handler.ts
@@ -1,21 +1,26 @@
 import { createFactory } from "hono/factory"
+import { zValidator } from "@hono/zod-validator"
+
+import { insertEntrySchema } from "@workers/database/schemas"
 import type { AppBindings } from "@workers/types"
 
 import * as HTTPStatus from "@workers/status-codes"
 // import * as HTTPPhrases from "@workers/status-phrases"
-
 import * as EntryService from "@workers/services/entry.service"
 
 const factory = createFactory<AppBindings>()
 
-export const createEntry = factory.createHandlers(async ctx => {
-  const user = ctx.get("user")
-  // const data = ctx.req.valid('')
+export const createEntry = factory.createHandlers(
+  zValidator("json", insertEntrySchema.omit({ userId: true })),
+  async ctx => {
+    const user = ctx.get("user")
+    const data = ctx.req.valid("json")
 
-  const entry = await EntryService.createEntry({
-    name: "",
-    userId: user.id
-  })
+    const entry = await EntryService.createEntry({
+      name: data.name,
+      userId: user.id
+    })
 
-  return ctx.json(entry, HTTPStatus.CREATED)
-})
+    return ctx.json(entry, HTTPStatus.CREATED)
+  }
+)
