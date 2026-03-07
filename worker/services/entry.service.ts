b0b63cca7470653748b66c2f398d9514aa59292b
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-03-07 21:57:36 +0800

    refac: split get entries logic

--- worker/services/entry.service.ts
+++ worker/services/entry.service.ts
@@ -1,8 +1,14 @@
 import { db } from "@workers/database/db"
 import { entry, insertEntrySchema } from "@workers/database/schemas"
+import { eq } from "drizzle-orm"
 import type z from "zod"
 
 type Entry = z.infer<typeof insertEntrySchema>
+
+export async function getEntries(userId: string) {
+  const entries = await db.select().from(entry).where(eq(entry.userId, userId))
+  return entries
+}
 
 export async function createEntry(values: Entry) {
   const createdEntry = await db
