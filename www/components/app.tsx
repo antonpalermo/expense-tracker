ab8cbf7c250c468833388e72e3dc54984fff63f2
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-03-07 22:40:06 +0800

    feat: entries

--- www/components/app.tsx
+++ www/components/app.tsx
@@ -1,9 +1,11 @@
 import EntryForm from "@client/components/entry/form"
+import Entries from "@client/components/entries"
 
 export default function App() {
   return (
     <div>
       <EntryForm />
+      <Entries />
     </div>
   )
 }
