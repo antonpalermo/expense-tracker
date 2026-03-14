df664fdccc775f13b2a4040173c23e8ee8ce50f3
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-03-14 23:57:40 +0800

    setup get ledgers apis

--- worker/services/ledger.service.ts
+++ worker/services/ledger.service.ts
@@ -1,5 +1,20 @@
 import { db } from "@workers/database/db"
 import { ledger, type LedgerInput } from "@workers/database/schemas/ledger"
+import { eq } from "drizzle-orm"
+
+export async function getLedgers(userId: string) {
+  try {
+    const result = await db
+      .select()
+      .from(ledger)
+      .where(eq(ledger.userId, userId))
+
+    return result
+  } catch (error) {
+    // TODO: improve logging here.
+    console.log(error)
+  }
+}
 
 export async function createLedger(input: LedgerInput) {
   try {
