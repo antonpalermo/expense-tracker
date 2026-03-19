e21d0c7413516985d73a95c9f76aea19f889e367
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-03-19 12:32:25 +0800

    re-generate migrations

--- drizzle/migrations/0000_omniscient_kinsey_walden.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `transaction` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `transaction_id_unique` ON `transaction` (`id`);
