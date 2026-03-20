61d56c9a739d6d5596248646d313fe0688749f9f
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-03-20 23:00:54 +0800

    migrate to neon-http

--- worker/database/db.ts
+++ worker/database/db.ts
@@ -1,8 +1,6 @@
 import { env } from "cloudflare:workers"
-import { Pool } from "@neondatabase/serverless"
-import { drizzle } from "drizzle-orm/neon-serverless"
+import { drizzle } from "drizzle-orm/neon-http"
 
 import * as schema from "@workers/database/schemas"
 
-const pool = new Pool({ connectionString: env.DABASE_URL })
-export const db = drizzle({ client: pool, schema })
+export const db = drizzle(env.DATABASE_URL, { schema })
