634cdc7faebc4d0f7ae8ae3dbb2a8b8e0d317113
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-04-01 00:09:06 +0800

    fix: auth layout issue

--- www/routes/_dashboardLayout/route.tsx
+++ www/routes/_dashboardLayout/route.tsx
@@ -1,7 +1,11 @@
 import * as React from "react"
 import { createFileRoute, Outlet } from "@tanstack/react-router"
 
-import { SidebarInset, SidebarTrigger } from "@client/components/ui/sidebar"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger
+} from "@client/components/ui/sidebar"
 import AppSidebar from "@client/components/app-sidebar"
 
 export const Route = createFileRoute("/_dashboardLayout")({
@@ -11,17 +15,19 @@ export const Route = createFileRoute("/_dashboardLayout")({
 function RouteComponent() {
   return (
     <React.Fragment>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+            </div>
+          </header>
+          <div className="px-4">
+            <Outlet />
           </div>
-        </header>
-        <div className="px-4">
-          <Outlet />
-        </div>
-      </SidebarInset>
+        </SidebarInset>
+      </SidebarProvider>
     </React.Fragment>
   )
 }
