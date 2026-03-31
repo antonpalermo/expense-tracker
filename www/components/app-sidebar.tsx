3910c91bcf1fbfc6709d0e596b635c34ef6c8722
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-03-31 22:50:30 +0800

    setup main nav

--- www/components/app-sidebar.tsx
+++ www/components/app-sidebar.tsx
@@ -1,15 +1,26 @@
 import * as React from "react"
-import { Sidebar, SidebarHeader } from "./ui/sidebar"
+import { IconDashboard, IconReceiptDollar } from "@tabler/icons-react"
+
+import { Sidebar, SidebarContent, SidebarHeader } from "./ui/sidebar"
 import LedgerSwitcher from "./ledger-switcher"
+import MainNav, { type NavItem } from "./main-nav"
 
 export default function AppSidebar({
   ...props
 }: React.ComponentProps<typeof Sidebar>) {
+  const mainNavigation: NavItem[] = [
+    { label: "Dashboard", url: "/", icon: IconDashboard },
+    { label: "Entries", url: "/entries", icon: IconReceiptDollar }
+  ]
+
   return (
     <Sidebar variant="inset" {...props}>
       <SidebarHeader>
         <LedgerSwitcher />
       </SidebarHeader>
+      <SidebarContent>
+        <MainNav items={mainNavigation} />
+      </SidebarContent>
     </Sidebar>
   )
 }
