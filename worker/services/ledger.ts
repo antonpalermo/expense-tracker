5e6105cc200540614d42c18f579186392619eb6c
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-04-07 17:51:11 +0800

    wip: ledger details

--- worker/services/ledger.ts
+++ worker/services/ledger.ts
@@ -1,5 +1,13 @@
+import { eq } from "drizzle-orm"
+
+import { db } from "@workers/database/db"
+import { entry, ledger } from "@workers/database/schemas"
+
 export class LedgerService {
-  getEntries() {
-    return "example that will get ledger entries"
+  async getLedger(id: string) {
+    return await db
+      .select()
+      .from(ledger)
+      .fullJoin(entry, eq(entry.ledgerId, id))
   }
 }
