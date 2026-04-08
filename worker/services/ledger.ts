c9a4ca5a6f68881b0762fbcf0b990f55ffc06254
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-04-08 22:15:33 +0800

    wip: update get ledger by id endpoint

--- worker/services/ledger.ts
+++ worker/services/ledger.ts
@@ -1,13 +1,13 @@
 import { eq } from "drizzle-orm"
 
 import { db } from "@workers/database/db"
-import { entry, ledger } from "@workers/database/schemas"
+import { ledger as ledgerSchema } from "@workers/database/schemas"
 
 export class LedgerService {
   async getLedger(id: string) {
-    return await db
-      .select()
-      .from(ledger)
-      .fullJoin(entry, eq(entry.ledgerId, id))
+    return await db.query.ledger.findFirst({
+      where: eq(ledgerSchema.id, id),
+      with: { entries: true } // <- only return entries for the past 30 days.
+    })
   }
 }
