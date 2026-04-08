f19d8300b9baa6b2d5151a58017116d732a2b9f1
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-04-08 22:34:13 +0800

    wip: desplay entries

--- www/routes/_dashboardLayout/entries.tsx
+++ www/routes/_dashboardLayout/entries.tsx
@@ -2,7 +2,9 @@ import { createEntryDialogHandle } from "@client/components/dialog-registry"
 import EntryDialog from "@client/components/entry-dialog"
 import { Button } from "@client/components/ui/button"
 import { DialogTrigger } from "@client/components/ui/dialog"
+import useLedgers from "@client/hooks/use-ledgers"
 import { IconPlus } from "@tabler/icons-react"
+import { useQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/_dashboardLayout/entries")({
@@ -10,6 +12,27 @@ export const Route = createFileRoute("/_dashboardLayout/entries")({
 })
 
 function RouteComponent() {
+  const { data: ledgers } = useLedgers()
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["entries", ledgers?.default],
+    enabled: !!ledgers?.default,
+    queryFn: async () => {
+      const request = await fetch(`/api/ledgers/${ledgers?.default}`)
+      if (!request.ok) {
+        return null
+      }
+      return await request.json()
+    }
+  })
+
+  if (isPending) {
+    return null
+  }
+
+  if (isError) {
+    return null
+  }
+
   return (
     <div>
       <div className="w-full flex items-center justify-between">
@@ -25,6 +48,7 @@ function RouteComponent() {
         ></DialogTrigger>
       </div>
       <EntryDialog />
+      {JSON.stringify(data)}
     </div>
   )
 }
