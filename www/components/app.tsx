9d594608e75baecfcade9202916facbe5dd434ed
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-04-17 23:33:48 +0800

    refac: removed old entry form

--- www/components/app.tsx
+++ www/components/app.tsx
@@ -1,11 +1,9 @@
-import EntryForm from "@client/components/entry/form"
 import Entries from "@client/components/entries"
 import { Button } from "@client/components/ui/button"
 
 export default function App() {
   return (
     <div>
-      <EntryForm />
       <Entries />
       <Button>Sample</Button>
     </div>
