d402c643d61fd260276974acdc35b1a79814a151
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-05-18 22:33:39 +0800

    introduce a get entries route

--- app/components/entries.tsx
+++ app/components/entries.tsx
@@ -1,9 +1,32 @@
+import { useQuery } from "@tanstack/react-query"
 import DynamicForm from "./form"
 
+const getEntries = async () => {
+    const request = await fetch("/api/entries/jTIgiBp1Jz74oKtnJxNo")
+    if (!request.ok) {
+        throw new Error("unable to fetch all entries")
+    }
+    return await request.json()
+}
+
 export default function Entries() {
+    const { data, isError, isPending } = useQuery({
+        queryKey: ["entries"],
+        queryFn: getEntries
+    })
+
+    if (isError) {
+        return <span>Error</span>
+    }
+
+    if (isPending) {
+        return <span>loading</span>
+    }
+
     return (
         <div>
             <h1>Application Entries</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
             <DynamicForm />
         </div>
     )
--- worker/routes/entries.ts
+++ worker/routes/entries.ts
@@ -5,13 +5,15 @@ import { entriesInsertSchema, entriesTable } from "../database/schemas/entries"
 import type { Bindings } from "../bindings"
 
 import z from "zod"
+import { eq } from "drizzle-orm"
+import { formTable } from "@/database/schemas"
 
 type Entries = z.infer<typeof entriesInsertSchema>
 
 const routes = new Hono<Bindings>({ strict: false }).basePath("/entries")
 
-async function createTask(entries: Entries) {
-    const sanitizedPayload = entriesInsertSchema.safeParse(entries)
+async function createEntry(details: Entries) {
+    const sanitizedPayload = entriesInsertSchema.safeParse(details)
 
     if (!sanitizedPayload.success) {
         return
@@ -25,6 +27,31 @@ async function createTask(entries: Entries) {
     return entry
 }
 
+async function getEntries(formId: string) {
+    const formResult = await db.query.formTable.findFirst({
+        where: eq(formTable.id, formId),
+        columns: {
+            fields: true
+        },
+        with: {
+            entries: {
+                columns: {
+                    data: true
+                }
+            }
+        }
+    })
+
+    const fields = formResult?.fields?.map(field => {
+        const schema = [[field.uid, field.name]]
+        return Object.fromEntries(schema)
+    })
+
+    const entries = formResult?.entries.map(entry => ({ ...entry.data }))
+
+    return { fields, entries }
+}
+
 routes.on(["POST"], "/", async ctx => {
     const method = ctx.req.method
 
@@ -32,10 +59,18 @@ routes.on(["POST"], "/", async ctx => {
         case "POST": {
             const body = await ctx.req.json()
 
-            const task = await createTask(body)
+            const task = await createEntry(body)
             return ctx.json(task)
         }
     }
+})
+
+routes.get("/:id", async ctx => {
+    const { id } = ctx.req.param()
+
+    const entries = await getEntries(id)
+
+    return ctx.json(entries)
 })
 
 export default routes
