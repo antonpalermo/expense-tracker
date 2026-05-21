ebb5b739d63d3d5dcc584e5b16e42cebaf0fd899
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-05-21 17:42:47 +0800

    feat: display data

--- app/components/data-table.tsx
+++ app/components/data-table.tsx
@@ -1,14 +1,72 @@
+import {
+    flexRender,
+    getCoreRowModel,
+    useReactTable
+} from "@tanstack/react-table"
 import * as React from "react"
 
-export default function DataTable({ data }: { data: [] }) {
-    const column = React.useMemo(() => {
+export default function DataTable({ data }: { data: unknown[] }) {
+    const columns = React.useMemo(() => {
         if (!data.length) return []
+
+        const keys = Object.keys(data[0])
+
+        return keys.map(key => ({
+            accessorKey: key,
+            header: key.toUpperCase(),
+            cell: (info: unknown) => {
+                const value = info.getValue()
+
+                if (typeof value === "object") {
+                    return JSON.stringify(value)
+                }
+
+                return value?.toString()
+            }
+        }))
     }, [data])
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel()
+    })
 
     return (
         <div>
             <h2>Entries</h2>
-            {JSON.stringify(column)}
+            <table>
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                              header.column.columnDef.header,
+                                              header.getContext()
+                                          )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id}>
+                                    {flexRender(
+                                        cell.column.columnDef.cell,
+                                        cell.getContext()
+                                    )}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
