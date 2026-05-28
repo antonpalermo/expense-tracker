a973903c927c3b53498f9d070d3bd73f923f1475
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-05-28 22:50:23 +0800

    wip: get entry data

--- worker/routes/entries.ts
+++ worker/routes/entries.ts
@@ -2,13 +2,36 @@ import { Hono } from "hono"
 import { db } from "../database/db"
 import { entriesInsertSchema, entriesTable } from "../database/schemas/entries"
 
-import type { Bindings } from "../bindings"
+import type { Bindings, Field } from "../bindings"
 
 import z from "zod"
+import { eq } from "drizzle-orm"
+import { HTTPException } from "hono/http-exception"
 
 type Entries = z.infer<typeof entriesInsertSchema>
 
 const routes = new Hono<Bindings>({ strict: false }).basePath("/entries")
+
+function parseFields(fields: Field[]): Record<string, string>[] {
+    return fields
+        .map(field => [[field.uid, field.name]])
+        .map(result => Object.fromEntries(result))
+}
+
+function mapFields(
+    fields: Record<string, string>[],
+    data: Record<string, unknown>
+) {
+    return fields.reduce<Record<string, unknown>>((prev, curr) => {
+        Object.entries(curr).forEach(([key, value]) => {
+            if (typeof value === "string") {
+                prev[value] = data[key] ?? null
+            }
+        })
+
+        return prev
+    }, {})
+}
 
 async function createEntry(details: Entries) {
     const sanitizedPayload = entriesInsertSchema.safeParse(details)
@@ -45,6 +68,7 @@ async function getEntries() {
     })
 
     const records = formResult?.entries.map(entry => ({ ...entry.data }))
+    console.log(records)
 
     const sanitizedResult = fields && Object.assign({}, ...fields)
 
@@ -64,6 +88,34 @@ async function getEntries() {
     return entries
 }
 
+async function getEntry(id: string) {
+    const result = await db.query.entriesTable.findFirst({
+        where: eq(entriesTable.id, id),
+        columns: {
+            data: true
+        },
+        with: {
+            form: {
+                columns: { fields: true }
+            }
+        }
+    })
+
+    if (!result) {
+        throw new HTTPException(404)
+    }
+
+    if (!result.form?.fields || !result.data) {
+        throw new Error("no fields available")
+    }
+
+
+    const fields = parseFields(result.form.fields)
+    const data = mapFields(fields, result.data)
+
+    return data
+}
+
 routes.on(["POST", "GET"], "/", async ctx => {
     const method = ctx.req.method
 
@@ -78,6 +130,24 @@ routes.on(["POST", "GET"], "/", async ctx => {
 
             const task = await createEntry(body)
             return ctx.json(task)
+        }
+    }
+})
+
+routes.on(["GET", "PATCH", "DELETE"], "/:id", async ctx => {
+    const method = ctx.req.method
+    const id = ctx.req.param("id")
+
+    switch (method) {
+        case "GET": {
+            const entry = await getEntry(id)
+            return ctx.json(entry)
+        }
+        case "PATCH": {
+            return ctx.json({ msg: "Hello from patch" + id })
+        }
+        case "DELETE": {
+            return ctx.json({ msg: "Hello from delete" + id })
         }
     }
 })
