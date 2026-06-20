24cc1a1885a10667680512553e7f7680caa82bbf
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-06-20 22:56:43 +0800

    refac: use useAppForm

--- app/hooks/form.ts
+++ app/hooks/form.ts
@@ -1,12 +1,34 @@
 import { createFormHook, createFormHookContexts } from "@tanstack/react-form"
-import Input from "../components/input"
+import { Input } from "@/components/ui/input"
+import {
+    FieldSet,
+    FieldGroup,
+    FieldLabel,
+    FieldTitle,
+    FieldError,
+    FieldLegend,
+    FieldContent,
+    FieldSeparator,
+    FieldDescription,
+    Field as MainField
+} from "@/components/ui/field"
 
 export const { fieldContext, formContext, useFieldContext, useFormContext } =
     createFormHookContexts()
 
 export const { useAppForm } = createFormHook({
     fieldComponents: {
-        Input
+        Input,
+        FieldSet,
+        FieldGroup,
+        FieldLabel,
+        FieldTitle,
+        FieldError,
+        FieldLegend,
+        FieldContent,
+        FieldSeparator,
+        FieldDescription,
+        MainField
     },
     formComponents: {},
     fieldContext,
