7ff3df55bcaf4f204885b770d251a6f7610616f5
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-06-22 22:51:56 +0800

    fix: entries query invalidation

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -14,7 +14,7 @@ import EntryDialog from "@/components/entries/dialog"
 
 export default function App() {
     const { data, isError, isPending } = useQuery<Entry[]>({
-        queryKey: entriesKeys.all,
+        queryKey: [entriesKeys.all],
         queryFn: getEntries
     })
 
