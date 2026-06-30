5fc7362dea4369ceba2997eac5474e83718860f8
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-06-30 18:00:43 +0800

    setup entries update route

--- worker/routes/entries.ts
+++ worker/routes/entries.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono"
 import type { HonoBindings } from "../index"
 
 import { validate } from "@/lib/validator"
-import { insertEntriesSchema } from "@/database/schemas"
+import { insertEntriesSchema, updateEntriesSchema } from "@/database/schemas"
 
 import * as HTTPStatus from "@/status-codes"
 // import * as HTTPPhrases from "@/status-phrases"
@@ -19,6 +19,11 @@ routes
         const data = ctx.req.valid("json")
         const newEntry = await EntriesService.create(data)
         return ctx.json(newEntry, HTTPStatus.CREATED)
+    })
+    .patch("/:id", validate("json", updateEntriesSchema), ctx => {
+        const method = ctx.req.method
+        const id = ctx.req.param("id")
+        return ctx.json({ msg: "hello from " + method + id })
     })
 
 routes.on(["GET", "PATCH", "DELETE"], "/:id", async ctx => {
