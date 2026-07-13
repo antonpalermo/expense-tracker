91ce01846495e756984dc3dc461b72243c6bfa9c
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-07-13 17:51:55 +0800

    wip: auth handler

--- worker/lib/auth.ts
+++ worker/lib/auth.ts
@@ -1,7 +1,12 @@
-import { betterAuth } from "better-auth"
 import { env } from "cloudflare:workers"
+import { betterAuth } from "better-auth"
+import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { db } from "@/database/db"
 
 export const auth = betterAuth({
     baseURL: env.BETTER_AUTH_URL,
-    secret: env.BETTER_AUTH_SECRET
+    secret: env.BETTER_AUTH_SECRET,
+    database: drizzleAdapter(db, {
+        provider: "sqlite"
+    })
 })
