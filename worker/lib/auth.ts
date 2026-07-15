a1f5bac5e5979e5a6a4131bead45dd7892ed2a4c
Anton Angelo N. Palermo <anton.palermo@dragonpay.ph>
2026-07-15 23:07:51 +0800

    wip: auth generate db schema

--- worker/lib/auth.ts
+++ worker/lib/auth.ts
@@ -3,10 +3,13 @@ import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { db } from "@/database/db"
 
+import * as authSchema from "@/database/schemas/auth"
+
 export const auth = betterAuth({
     baseURL: env.BETTER_AUTH_URL,
     secret: env.BETTER_AUTH_SECRET,
     database: drizzleAdapter(db, {
-        provider: "sqlite"
+        provider: "sqlite",
+        schema: authSchema
     })
 })
