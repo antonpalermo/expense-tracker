10f1e80bc01ab6777f6b3998ded3231868391660
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2026-07-19 22:36:30 +0800

    init tanstack router

--- app/main.tsx
+++ app/main.tsx
@@ -5,18 +5,26 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { TanStackDevtools } from "@tanstack/react-devtools"
 import { formDevtoolsPlugin } from "@tanstack/react-form-devtools"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { createRouter, RouterProvider } from "@tanstack/react-router"
 
 import "./index.css"
-
-import App from "@/components/app"
+import { routeTree } from "@/routeTree.gen"
 import { Toaster } from "@/components/ui/sonner"
 
 const client = new QueryClient()
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router
+    }
+}
 
 createRoot(document.getElementById("root") as HTMLElement).render(
     <StrictMode>
         <QueryClientProvider client={client}>
-            <App />
+            <RouterProvider router={router} />
             <ReactQueryDevtools />
             <Toaster />
         </QueryClientProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,23 @@ import tailwindcss from "@tailwindcss/vite"
 
 import { devtools } from "@tanstack/devtools-vite"
 import { cloudflare } from "@cloudflare/vite-plugin"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), devtools(), tailwindcss(), cloudflare()],
+    plugins: [
+        tanstackRouter({
+            target: "react",
+            autoCodeSplitting: true,
+            routesDirectory: "./app/routes",
+            generatedRouteTree: "./app/routeTree.gen.ts",
+            routeFileIgnorePrefix: "-"
+        }),
+        react(),
+        devtools(),
+        tailwindcss(),
+        cloudflare()
+    ],
     resolve: {
         tsconfigPaths: true
     }
