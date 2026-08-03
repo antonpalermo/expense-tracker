6c4e2a3735f2f59e1b0e5f53d9dbf1c7292c1736
Anton Angelo N. Palermo <ANPBusinessmail@gmail.com>
2026-08-03 22:54:55 +0800

    added session middleware

--- worker/index.ts
+++ worker/index.ts
@@ -2,12 +2,33 @@ import { Hono } from 'hono'
 import authRoutes from '@/routes/auth'
 import taskRoutes from '@/routes/entries'
 import formRoutes from '@/routes/form'
+import { auth } from './lib/auth'
 
 export type HonoBindings = {
     Bindings: CloudflareBindings
+    Variables: {
+        user: typeof auth.$Infer.Session.user | null
+        session: typeof auth.$Infer.Session.session | null
+    }
 }
 
 const app = new Hono<HonoBindings>({ strict: false }).basePath('/api')
+
+app.use('*', async (c, next) => {
+    const session = await auth.api.getSession({ headers: c.req.raw.headers })
+
+    if (!session) {
+        c.set('user', null)
+        c.set('session', null)
+        await next()
+        return
+    }
+
+    c.set('user', session.user)
+    c.set('session', session.session)
+    await next()
+})
+
 const routes = [authRoutes, formRoutes, taskRoutes]
 
 routes.forEach(route => app.route('/', route))
